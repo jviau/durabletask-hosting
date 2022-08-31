55362455cef9dbcf9fd805895f3bf802585e8b31
Jonathan Rajotte <jraj@microsoft.com>
2022-08-31 11:57:14 -0400

    Expose new ErrorPropagationMode option to allow controlling the error propagation behavior when an activity or orchestration fails with an unhandled exception

--- releasenotes.md
+++ releasenotes.md
@@ -1,8 +1,4 @@
-# Release 2.1.6-preview
+# Release 2.1.7-preview
 
-- Fix `ObjectDisposedException` race condition during orchestration middleware
-  - The fix was to remove the attempt to re-use `IServiceProvider` during sessions of the same orchestration.
-  - The above would only impact if the DTFx orchestration/activity was registered with "Scoped" lifetime.
-  - Doing the above is **strongly not recommended**. It would lead to undeterministic statefulness of your orchestration or activity.
-- Fixed generic type parsing to properly handle nested generics when there are multiple generic arguments.
-- Fix `WrapperOrchestrationContext.IsReplaying` not properly updating.
+- Upgrade to version 2.11.0 of DurableTask.Core
+- Expose new ErrorPropagationMode option to allow controlling the error propagation behavior when an activity or orchestration fails with an unhandled exception
--- src/DurableTask.Hosting/src/Options/TaskHubOptions.cs
+++ src/DurableTask.Hosting/src/Options/TaskHubOptions.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Jacob Viau. All rights reserved.
 // Licensed under the APACHE 2.0. See LICENSE file in the project root for full license information.
 
+using System;
 using DurableTask.Core;
+using DurableTask.Core.Exceptions;
 
 namespace DurableTask.Hosting.Options
 {
@@ -21,5 +23,22 @@ namespace DurableTask.Hosting.Options
         /// <see cref="TaskActivityDispatcher.IncludeDetails"/> and <see cref="TaskOrchestrationDispatcher.IncludeDetails"/>.
         /// </summary>
         public IncludeDetails IncludeDetails { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error propagation behavior when an activity or orchestration fails with an unhandled exception.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Use caution when making changes to this property over the lifetime of an application. In-flight orchestrations
+        /// could fail unexpectedly if there is any logic that depends on a particular behavior of exception propagation.
+        /// For example, setting <see cref="ErrorPropagationMode.UseFailureDetails"/> causes
+        /// <see cref="OrchestrationException.FailureDetails"/> to be populated in <see cref="TaskFailedException"/> and
+        /// <see cref="SubOrchestrationFailedException"/> but also causes the <see cref="Exception.InnerException"/> 
+        /// property to be <c>null</c> for these exception types.
+        /// </para><para>
+        /// This property must be set before the worker is started. Otherwise it will have no effect.
+        /// </para>
+        /// </remarks>
+        public ErrorPropagationMode ErrorPropagationMode { get; set; }
     }
 }
