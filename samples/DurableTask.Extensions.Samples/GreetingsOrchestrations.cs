754c227e31880e61fe630aea4f3ac9e016e2820e
Jacob Viau <javia@microsoft.com>
2022-10-07 13:07:03 -0700

    Add samples for Vio.DurableTask.Extensions (#40)
    * Add samples for Vio.DurableTask.Extensions

---
+++ samples/DurableTask.Extensions.Samples/GreetingsOrchestrations.cs
@@ -0,0 +1,77 @@
+// Copyright (c) Jacob Viau. All rights reserved.
+// Licensed under the APACHE 2.0. See LICENSE file in the project root for full license information.
+
+using DurableTask.DependencyInjection;
+using DurableTask.Extensions.Abstractions;
+
+namespace DurableTask.Extensions.Samples;
+
+internal class GreetingsOrchestration : IOrchestrationRequest<string>
+{
+    public TaskOrchestrationDescriptor GetDescriptor() => new(typeof(Handler));
+
+    public class Handler : OrchestrationBase<GreetingsOrchestration, string>
+    {
+        protected override async Task<string> RunAsync(GreetingsOrchestration input)
+        {
+            string user = await Context.SendAsync(new GetUserActivity());
+            await Context.SendAsync(new SendGreetingActivity(user));
+            return user;
+        }
+    }
+}
+
+internal class GetUserActivity : IActivityRequest<string>
+{
+    public TaskActivityDescriptor GetDescriptor() => new(typeof(Handler));
+
+    public class Handler : ActivityBase<GetUserActivity, string>
+    {
+        private readonly IConsole _console;
+
+        public Handler(IConsole console)
+            => _console = console ?? throw new ArgumentNullException(nameof(console));
+
+        protected override Task<string> RunAsync(GetUserActivity input)
+        {
+            _console.WriteLine("What is your name?");
+            return Task.FromResult(_console.ReadLine());
+        }
+    }
+}
+
+internal sealed class SendGreetingActivity : IActivityRequest
+{
+    public SendGreetingActivity(string user) => User = user;
+
+    public string User { get; }
+
+    public TaskActivityDescriptor GetDescriptor() => new(typeof(Handler));
+
+    public class Handler : ActivityBase<SendGreetingActivity>
+    {
+        private readonly IConsole _console;
+
+        public Handler(IConsole console)
+            => _console = console ?? throw new ArgumentNullException(nameof(console));
+
+        /// <inheritdoc />
+        protected override async Task<Empty> RunAsync(SendGreetingActivity input)
+        {
+            string user = input.User;
+            if (!string.IsNullOrWhiteSpace(user) && user.Equals("TimedOut"))
+            {
+                _console.WriteLine("GetUser Timed out!!!");
+            }
+            else
+            {
+                _console.WriteLine($"Sending greetings to user: {user}...");
+                await Task.Delay(5 * 1000);
+                _console.WriteLine($"Greetings sent to {user}");
+            }
+
+            return Empty.Value;
+        }
+    }
+}
+
---
+++ samples/DurableTask.Extensions.Samples/StjDataConverter.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Jacob Viau. All rights reserved.
+// Licensed under the APACHE 2.0. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using DurableTask.Core.Serializing;
+
+namespace DurableTask.Extensions.Samples;
+
+/// <summary>
+/// DataConverter using System.Text.Json.
+/// </summary>
+internal class StjDataConverter : DataConverter
+{
+    private readonly JsonSerializerOptions _options;
+
+    public StjDataConverter(JsonSerializerOptions options = null)
+        => _options = options ?? new();
+
+    public override string Serialize(object value) => Serialize(value, false);
+
+    public override string Serialize(object value, bool formatted)
+        => JsonSerializer.Serialize(value, _options);
+
+    public override object Deserialize(string data, Type objectType)
+        => JsonSerializer.Deserialize(data, objectType, _options);
+}
